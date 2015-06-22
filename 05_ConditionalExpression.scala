994db28de396a65f41a216d3eb9ea40159c6382b
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-06-22 14:41:20 -0700

    Fixed a typo

--- 05_ConditionalExpression.scala
+++ 05_ConditionalExpression.scala
@@ -12,10 +12,12 @@ object ConditionalExpression
     println("printSign(-5.7): " + printSign(-5.7))
   }
 
-  // Define a function that takes one parameter of type int and returns
+  // Define a function that takes one parameter of type Int and returns
   // the value if it is greater than 0 or returns 0 if not
-  // This is equivalent to the terniary operator in other languages like Java and C
+  // This is equivalent to the ternary operator in other languages like Java and C
   def neverNegative(value: Int) = if (value > 0) value else 0
 
+  // Define a function that takes one parameter of type Double and returns
+  // if it is positive or negative
   def printSign(value: Double) = if (value < 0) "Negative" else "Positive"
 }
