4fb8ff599ce6a25e2d91a00f4d26adace20f8080
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-06-22 12:45:16 -0700

    Added comments to HelloWorld

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,10 +1,11 @@
-/**
- * Created by shubham on 6/22/15.
- */
+// For now, it's enough to know that "object" creates a Singleton
 object HelloWorld
 {
+  // Main function takes an array of strings and gives it the name "args"
+  // This is similar to Java's "public static void main(String[] args)"
   def main(args: Array[String])
   {
+  	// Unlike Java's System.out.println(), Scala has println()
     println("Hello World!")
   }
 }
