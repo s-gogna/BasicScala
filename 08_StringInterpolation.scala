3d54e9b4dcdee1e2d0af4acf89a4e90a73036d01
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-06-23 16:51:11 -0700

    Added clarification

--- 08_StringInterpolation.scala
+++ 08_StringInterpolation.scala
@@ -8,14 +8,14 @@ object StringInterpolation
 
     // Adding the 's' in front of a string literal like this allows
     // the programmer to add in variables names and expressions which 
-    // will be replaced (Scala version 2.10+)
+    // will be replaced ('s' is Scala version 2.10+)
     println( s"The value of alpha was interpolated as $alpha" )
     println( s"The value of beta was interpolated as $beta" )
     println( s"It can also do math! What is 3 * 12? ${3 * 12}" )
 
     // If you're familiar with C's printf or Java's string format,
     // Scala also supports that with 'f' instead of 's'. Additionally,
-    // it's also type safe (Scala version 2.10+)
+    // using 'f' is type safe ('f' is Scala version 2.10+)
     println( f"Displaying alpha as an integer ... $alpha%i" )
     println( f"Displaying alpha as with options for floats ... $alpha%2.5f" )
   }
