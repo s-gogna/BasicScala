3da45b175099bde5dbd85c4262fa9edcd94c26df
Shubham Gogna <s-gogna@users.noreply.github.com>
2015-06-24 16:27:24 -0700

    Added a section on anonymous functions without explicit types

--- 10_AnonymousFunctions.scala
+++ 10_AnonymousFunctions.scala
@@ -7,6 +7,11 @@ object AnonymousFunctions
     doSomethingToFive("After anonymous addTen, the value is ", (value: Int) => value + 10 )
     doSomethingToFive("After anonymous multiplyByTwenty, the value is ", (value: Int) => value * 20 )
     doSomethingToFive("After anonymous subtractThree, the value is ", (value: Int) => value - 3 )
+
+    // The compiler is smart enough that we also don't need to include the type
+    // in the anonymous function because it will infer it from the function we
+    // are passing it to
+    doSomethingToFive("After anonymous multiplyByThree, the value is ", (value) => value * 3 )
   }
 
   // This function, "doSomethingToFive", takes another function, "funct", which
